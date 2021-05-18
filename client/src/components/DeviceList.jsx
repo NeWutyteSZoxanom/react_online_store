840bcf8ff1c06026a7626d324575936edd8a5a39
unknown <NeWutyteSZoxanom>
2021-05-19 00:56:56 +0500

    config

--- client/src/components/DeviceList.jsx
+++ client/src/components/DeviceList.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
-
+s;
 const DeviceList = () => {
   return (
     <div>
-      <Paper elevation={3} className="decise" />
+      <Paper elevation={3} className="decise" square />
     </div>
   );
 };
